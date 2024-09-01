e99173bf716bcfb05f8e0fcd7a7c7237ee0f3211
enokawa <enokawa00@gmail.com>
2024-09-02 00:30:23 +0900

    Fix count

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import Box from "@mui/material/Box";
 import { Grid2 as Grid } from "@mui/material";
 import "./App.css";
 
-const checkboxes = 40;
+const columns = 8;
+const checkboxes = columns * 100;
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
       <h1 className="text-3xl">BUSY BOARD</h1>
       <div className="checkboxes mt-6">
         <Box>
-          <Grid container columns={8} alignItems="center">
+          <Grid container columns={columns} alignItems="center">
             {Array.from(Array(checkboxes)).map((_, index) => (
               <Grid key={index} size={1}>
                 <Checkbox />

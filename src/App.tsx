09bfc0772ed2870e4f5248a3d48bf9cc8ad8a277
enokawa <enokawa00@gmail.com>
2024-09-02 00:21:05 +0900

    Add Checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import { Checkbox } from "@mui/material";
+import Box from "@mui/material/Box";
+import { Grid2 as Grid } from "@mui/material";
 import "./App.css";
+
+const checkboxes = 40;
 
 function App() {
   return (
     <>
       <h1 className="text-3xl">BUSY BOARD</h1>
-      <div className="checkboxes">
-        <Checkbox />
-        <Checkbox />
-        <Checkbox />
+      <div className="checkboxes mt-6">
+        <Box>
+          <Grid container columns={8} alignItems="center">
+            {Array.from(Array(checkboxes)).map((_, index) => (
+              <Grid key={index} size={1}>
+                <Checkbox />
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
       </div>
     </>
   );

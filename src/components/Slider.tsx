0a3c5d4404413238e6ea9f14f5f25b2ffeb43f1a
enokawa <enokawa00@gmail.com>
2024-09-12 22:58:47 +0900

    Add Slider

---
+++ src/components/Slider.tsx
@@ -0,0 +1,7 @@
+import { Slider as MuiSlider } from "@mui/material";
+
+export const Slider = () => {
+  return <MuiSlider defaultValue={30} color="warning" />;
+};
+
+export default Slider;
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export * from "./Checkbox";
 export * from "./ProgressBar";
+export * from "./Slider";
